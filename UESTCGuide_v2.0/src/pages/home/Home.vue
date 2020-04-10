dad042f0d8b7e7c14485564a08abca2c4bbf224f
Liu-xc <1392166232@qq.com>
2020-04-10 15:35:35 +0800

    prepare home-main

--- UESTCGuide_v2.0/src/pages/home/Home.vue
+++ UESTCGuide_v2.0/src/pages/home/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <home-header></home-header>
     <home-navbar :navList="navList"></home-navbar>
+    <home-main></home-main>
     <div style="height: 80rem"></div>
   </div>
 </template>
@@ -9,12 +10,14 @@
 <script>
 import HomeHeader from '@/pages/home/components/HomeHeader'
 import HomeNavbar from '@/pages/home/components/HomeNavbar'
+import HomeMain from '@/pages/home/components/main/HomeMain'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeNavbar
+    HomeNavbar,
+    HomeMain
   },
   data () {
     return {
---
+++ UESTCGuide_v2.0/src/pages/home/components/main/HomeMain.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+export default {
+  name: 'HomeMain'
+}
+</script>
+
+<style scoped lang="stylus"></style>
